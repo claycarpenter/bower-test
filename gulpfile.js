0acd3073d13f2540140e3c0eb177b1cf34a8a7ba
Clay Carpenter <claycarpenter@gmail.com>
2016-06-07 23:58:27 -0400

    Very basic gulp-babel build system.
    Adds test ES6 script.

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp'),
+      babel = require('gulp-babel');
+
+gulp.task('js', function() {
+  return gulp.src('src/**/*.js')
+      // .pipe(print())
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(gulp.dest('build'));               
+});
+
---
+++ src/es6-test.js
@@ -0,0 +1,7 @@
+
+(function() {
+  'use strict';
+
+  const test = 1;
+  let templateLiteral = `The value is ${test}.`;
+}());
