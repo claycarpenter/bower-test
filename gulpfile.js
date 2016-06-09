777da2497920f28397dcc899d2701ee850cb3a1e
Clay Carpenter <claycarpenter@gmail.com>
2016-06-08 22:43:34 -0400

    Enhances build system.
    Adds clean, default, and build tasks.
    
    Adds run-sequence and del module deps to provide support for ordered gulp tasks and cleaning files, respectively.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp'),
-      babel = require('gulp-babel');
+      babel = require('gulp-babel'),
+      del = require('del'),
+      runSequence = require('run-sequence');
 
 const Directories = {
   Source: 'src',
@@ -7,12 +9,18 @@ const Directories = {
 };
 
 gulp.task('js', function() {
-  return gulp.src('${Directories.Source}/**/*.js')
-      // .pipe(print())
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(gulp.dest(Directories.Distributable));
+  return gulp
+    .src(`${Directories.Source}/**/*.js`)
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(gulp.dest(Directories.Distributable));
 });
 
-// Add:
-// - default
-// - clean
+gulp.task('clean', function() {
+  return del([Directories.Distributable]);
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean', 'js', callback);
+});
+
+gulp.task('default', ['clean']);
