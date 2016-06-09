352a496933b8f33145906eb57dd26de75b0e3544
Clay Carpenter <claycarpenter@gmail.com>
2016-06-08 22:31:42 -0400

    Converts directory config to external vars.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const gulp = require('gulp'),
       babel = require('gulp-babel');
 
+const Directories = {
+  Source: 'src',
+  Distributable: 'dist',
+};
+
 gulp.task('js', function() {
-  return gulp.src('src/**/*.js')
+  return gulp.src('${Directories.Source}/**/*.js')
       // .pipe(print())
       .pipe(babel({ presets: ['es2015'] }))
-      .pipe(gulp.dest('build'));               
+      .pipe(gulp.dest(Directories.Distributable));
 });
 
+// Add:
+// - default
+// - clean
