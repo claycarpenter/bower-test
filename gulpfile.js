3587354418d59c65e7990928b555d75bb69176c0
Clay Carpenter <claycarpenter@gmail.com>
2016-06-11 10:30:15 -0400

    Initial working Browserify/Babelify build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp'),
       babel = require('gulp-babel'),
+      babelify = require('babelify'),
       del = require('del'),
       runSequence = require('run-sequence'),
-      concat = require('gulp-concat');
+      concat = require('gulp-concat'),
+      browserify = require('browserify'),
+      source = require('vinyl-source-stream'),
+      buffer = require('vinyl-buffer');
 
 const Directories = {
   Source: 'src',
@@ -17,12 +21,25 @@ gulp.task('js', function() {
     .pipe(gulp.dest(Directories.Distributable));
 });
 
+// const bundler = browserify('src/pendingButton.module.js', {debug: true});
+
+const bundler = browserify('src/pendingButton.module.js', {debug: true})
+  .transform(babelify, {presets: ["es2015"]});
+
+gulp.task('browserify-js', function() {
+  return bundler.bundle()
+    .on('error', function(err) { console.error(err); this.emit('end'); })
+    .pipe(source('all.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(Directories.Distributable));
+});
+
 gulp.task('clean', function() {
   return del([Directories.Distributable]);
 });
 
 gulp.task('build', function(callback) {
-  runSequence('clean', 'js', callback);
+  runSequence('clean', 'browserify-js', callback);
 });
 
 gulp.task('default', ['build']);
