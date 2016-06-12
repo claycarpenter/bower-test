dc52dfe7b0cdf4126be5d1a29388cafcd99ef524
Clay Carpenter <claycarpenter@gmail.com>
2016-06-11 21:31:18 -0400

    Adds support for configurable waiting style classes.

--- src/js/pending-button.directive.js
+++ src/js/pending-button.directive.js
@@ -4,8 +4,9 @@ export function mbmPendingButton() {
     restrict: 'A',
     require: 'mbmPendingButton',
     scope: {
+      actionReceiver: '&mbmPendingButton',
       isExternalEnabled: '=?pendingButtonEnabled',
-      actionReceiver: '&mbmPendingButton'
+      waitingClass: '@?pendingButtonWaitingClass',
     },
     controller: ['$scope', function($scope) {
       var vm = this,
@@ -35,6 +36,14 @@ export function mbmPendingButton() {
     link: function ($scope, element, attr, pendingButtonCtrl) {
       var isWaiting = false;
 
+      // Start with the default waiting class
+      var waitingClass = 'mbm-pending-button--waiting';
+
+      if (typeof $scope.waitingClass === 'string') {
+        // Override the default if a custom waiting class has been provided.
+        waitingClass = $scope.waitingClass;
+      }
+
       if (typeof $scope.isExternalEnabled === 'undefined') {
         // No value provided for external button enabled attribute; assume
         // button is always considered externally enabled.
@@ -58,8 +67,8 @@ export function mbmPendingButton() {
         // Execute the action, and capture the (expected) returned promise.
         var actionQ = $scope.actionReceiver();
 
-        // Add loading animation.
-        element.addClass('mbm-pending-button--loading');
+        // Add waiting style.
+        element.addClass(waitingClass);
 
         // Set waiting flag, and apply scope to update isEnabled watch.
         isWaiting = true;
@@ -75,9 +84,9 @@ export function mbmPendingButton() {
             pendingButtonCtrl.updateState('error');
           })
           .finally(function() {
-            // Clear the loading animation and waiting flag.
+            // Clear the waiting style and flag.
             isWaiting = false;
-            element.removeClass('mbm-pending-button--loading');
+            element.removeClass(waitingClass);
           });
       });
     }
